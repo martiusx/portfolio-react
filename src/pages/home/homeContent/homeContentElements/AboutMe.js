64757ad42ae3b0d197a771065ea89c3ddf01a28b
martiusx <Maciej.Cis@o2.pl>
2022-12-27 21:41:06 +0100

    about me section updated - styles is not completed

--- src/pages/home/homeContent/homeContentElements/AboutMe.js
+++ src/pages/home/homeContent/homeContentElements/AboutMe.js
@@ -1,14 +1,35 @@
+import {useState} from "react";
+
 const AboutMe = function() {
+
+    const [readMore, setReadMore] = useState(false);
+
     return(
         <div className="content_aboutMe">
-            <p>
-                Elit occaecat labore anim voluptate laborum cillum. 
-                Pariatur est enim eiusmod in tempor culpa sint irure sit. 
-                Ea aute irure cupidatat tempor dolor elit. 
-                Nostrud laboris nostrud et laborum veniam ullamco sunt magna.
-                 Quis deserunt nulla proident qui laborum irure dolor enim laboris.
+            <p className="content_aboutMe_visible">
+                i'm a novice programmer, seeking the job where i can take a first commercial
+                 experience, and develop in the industry.            
             </p>
+            <div className="content_aboutMe_hidden">
+                <p>Pariatur officia ea ipsum excepteur dolore commodo quis 
+                    irure eiusmod minim nisi magna in sint. Elit laborum nostrud officia 
+                    magna ad voluptate et. Commodo veniam enim ullamco minim. Ea eu cillum aliqua 
+                    consequat ipsum voluptate in irure ut elit consequat nisi adipisicing. Sint voluptate 
+                    qui quis quis consectetur laborum mollit adipisicing aute consectetur. Ipsum amet 
+                    labore culpa amet et ut aliqua in laborum eiusmod sunt laborum in.
+                </p>
+                <div 
+                    className={readMore? 'content_aboutMe_button content_aboutMe_button--on' : 'content_aboutMe_button content_aboutMe_button--off'}>
+                    <span 
+                        onClick={() => {
+                            setReadMore(!readMore)
+                            }}>
+                        {readMore? 'less' : 'Read more about me'}
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
+
 export default AboutMe;
