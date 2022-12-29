61de0a15f80280392dccfb4398be688202f4b692
martiusx <Maciej.Cis@o2.pl>
2022-12-30 00:02:13 +0100

    collection slider update

--- src/pages/home/homeContent/homeContentElements/MyCollectionsImages.js
+++ src/pages/home/homeContent/homeContentElements/MyCollectionsImages.js
@@ -2,7 +2,9 @@ import image1 from './assets/OddamWDobreRece.png';
 import image2 from './assets/BestShop.png';
 import image3 from './assets/CarWashing.png';
 import image4 from './assets/Verti.png';
+import image5 from './assets/Ajmer.png';
+import image6 from './assets/FootLocker.png';
 
-const images = [image1, image2, image3, image4];
+const images = [image1, image2, image3, image4, image5, image6];
 
 export default images;
