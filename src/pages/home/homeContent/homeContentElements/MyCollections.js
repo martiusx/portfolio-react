c5932d6fc18cfa0301587b267eb946c6a449e303
martiusx <Maciej.Cis@o2.pl>
2023-01-03 21:43:37 +0100

    update of my collection section

--- src/pages/home/homeContent/homeContentElements/MyCollections.js
+++ src/pages/home/homeContent/homeContentElements/MyCollections.js
@@ -13,11 +13,20 @@ const MyCollection = function() {
 
     return(
         <div ref={slider} className="content_myCollection">
+             <span>My works</span>
             <motion.div  className="content_myCollection_slider">
                 <motion.div drag="x" dragConstraints={{ right:0, left: - width }} className="content_myCollection_slider--wrapper">
-                    {images.map((image) => {
+                    <motion.div className="content_myCollection_slider--slide content_myCollection_slider--text">
+                    <h3>
+                        Eiusmod esse mollit enim velit quis sunt culpa officia ex duis 
+                        officia sunt irure. Veniam non excepteur sint sunt id dolor pariatur 
+                        deserunt Lorem fugiat pariatur consectetur nostrud. Sunt nostrud culpa 
+                        elit laborum in pariatur aute pariatur amet labore commodo officia.
+                    </h3>
+                    </motion.div>
+                    {images.map((image, index) => {
                         return(
-                            <motion.div className="content_myCollection_slider--slide"><img src={image} alt=""/></motion.div>
+                            <motion.div key={index} className="content_myCollection_slider--slide"><img src={image} alt=""/></motion.div>
                         )
                     })}
                 </motion.div>
