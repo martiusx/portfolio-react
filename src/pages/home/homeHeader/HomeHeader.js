b240bb940f836da00240a6cbd10f723756771e1e
martiusx <Maciej.Cis@o2.pl>
2023-01-15 16:00:10 +0100

    header section updated

--- src/pages/home/homeHeader/HomeHeader.js
+++ src/pages/home/homeHeader/HomeHeader.js
@@ -1,13 +1,13 @@
 import HomeHeaderBackground from "./homeHeaderElements/HomeHeaderBackground";
 import HomeHeaderTitle from "./homeHeaderElements/HomeHeaderTitle";
 
-const HomeHeader = function() {
-    return(
-        <header>
-            <HomeHeaderBackground/>
-            <HomeHeaderTitle/>
-        </header>
-    )
-}
+const HomeHeader = function () {
+  return (
+    <header>
+      <HomeHeaderBackground />
+      <HomeHeaderTitle />
+    </header>
+  );
+};
 
 export default HomeHeader;
--- src/pages/home/homeHeader/homeHeaderElements/HomeHeaderTitle.js
+++ src/pages/home/homeHeader/homeHeaderElements/HomeHeaderTitle.js
@@ -1,9 +1,24 @@
-const HomeHeaderTitle = function() {
-    return(
-        <div className="header_title">
-            <h1>Hello<span>I'm</span>Maciek</h1>
-        </div>
-    )
-}
+import { useRef } from "react";
+import { useInView } from "framer-motion";
+
+const HomeHeaderTitle = function () {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <div ref={ref} className="header_title" id="header__title-id">
+      <h1
+        style={{
+          transform: isInView ? "none" : "translateY(200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+        }}
+      >
+        Hello
+        <span>I'm</span>Maciek
+      </h1>
+    </div>
+  );
+};
 
 export default HomeHeaderTitle;
